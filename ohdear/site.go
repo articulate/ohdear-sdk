6588a92a72e0d26b4a5e1de748199a22dd041053
Chaz <chaz@plaidpotion.com>
2018-10-25 10:44:18 -0400

    slice of pointer to struct in site

--- ohdear/site.go
+++ ohdear/site.go
@@ -6,14 +6,14 @@ import (
 )
 
 type Site struct {
-	Id                    int     `json:"id,omitempty"`
-	Url                   string  `json:"url,omitempty"`
-	TeamId                int     `json:"team_id,omitempty"`
-	LatestRunDate         string  `json:"latest_run_date,omitempty"`
-	SummarizedCheckResult string  `json:"summarized_check_result,omitempty"`
-	CreatedAt             string  `json:"created_at,omitempty"`
-	UpdatedAt             string  `json:"updates_at,omitempty"`
-	Checks                []Check `json:"checks,omitempty"`
+	Id                    int      `json:"id,omitempty"`
+	Url                   string   `json:"url,omitempty"`
+	TeamId                int      `json:"team_id,omitempty"`
+	LatestRunDate         string   `json:"latest_run_date,omitempty"`
+	SummarizedCheckResult string   `json:"summarized_check_result,omitempty"`
+	CreatedAt             string   `json:"created_at,omitempty"`
+	UpdatedAt             string   `json:"updates_at,omitempty"`
+	Checks                []*Check `json:"checks,omitempty"`
 }
 
 type SiteService struct {
